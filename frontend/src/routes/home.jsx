239c13e72749194ea807a027789c78a61b610723
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-02-02 21:51:29 +0000

    carrega destinos da api

--- frontend/src/routes/home.jsx
+++ frontend/src/routes/home.jsx
@@ -1,18 +1,28 @@
 import { Component } from "react";
+import axios from 'axios';
 import Oferta from '../components/Oferta';
 
 class Home extends Component {
+    state = {
+        destinos: []
+    }
+    componentDidMount() {
+        axios.get('http://localhost:4000/destinos')
+            .then(res => {
+                this.setState(res.data)
+            })
+
+    }
+
     render() {
         return <section className="offers" id="offers">
             <div className="box-container">
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
-                <Oferta nome='Amesterdã' preco="3800" localizacao="Internacional - Holanda" />
+                {
+                    this.state.destinos
+                        .map(destino =>
+                            <Oferta key={destino.id} nome={destino.nome} preco={destino.preco} />
+                        )
+                }
             </div>
         </section>
     }
