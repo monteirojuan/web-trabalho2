5f5194876fe7faa1d22b29cfed9bdd1632871105
Juan Monteiro <juan.monteiro@jmonteiro.net>
2022-02-03 20:21:55 +0000

    adiciona lista de reservas

--- frontend/src/routes/reservas.jsx
+++ frontend/src/routes/reservas.jsx
@@ -1,9 +1,21 @@
 import { Component } from "react";
-
+import axios from "axios";
 class Reserva extends Component {
+    state = {
+        reservas: []
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:4000/reservas')
+            .then(res => {
+                this.setState(res.data)
+                console.log(res.data)
+            })
+
+    }
     render() {
         return <section className="offers" id="offers">
-            <div className="table">
+            <div className="table box-container">
                 <table className="content-table">
                     <thead>
                         <tr>
@@ -12,18 +24,23 @@ class Reserva extends Component {
                             <th>CPF</th>
                             <th>Destino</th>
                             <th>Data de partida</th>
-                            <th>Data de retorno</th> 
+                            <th>Data de retorno</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>{this.props.id}</td>
-                            <td>{this.props.nome}</td>
-                            <td>{this.props.cpf}</td>
-                            <td>{this.props.destino}</td>
-                            <td>{this.props.partida}</td>
-                            <td>{this.props.retorno}</td>
-                        </tr>
+                        {
+                            this.state.reservas.map(
+                                reserva =>
+                                    <tr>
+                                        <td>{reserva.id}</td>
+                                        <td>{reserva.passageiro.nome}</td>
+                                        <td>{reserva.passageiro.cpf}</td>
+                                        <td>{reserva.destino.nome}</td>
+                                        <td>{reserva.partida}</td>
+                                        <td>{reserva.retorno}</td>
+                                    </tr>
+                            )
+                        }
                     </tbody>
                 </table>
             </div>
