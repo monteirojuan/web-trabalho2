519f97a3a71bf63c16de6eb457d59aa6896009a5
Anne Caroline <76491504+Carol-Annie@users.noreply.github.com>
2022-02-03 10:53:06 -0300

    Update reservas.jsx

--- frontend/src/routes/reservas.jsx
+++ frontend/src/routes/reservas.jsx
@@ -3,8 +3,29 @@ import { Component } from "react";
 class Reserva extends Component {
     render() {
         return <section className="offers" id="offers">
-            <div className="box-container">
-                botar uma lista com todas as reservas do sistema
+            <div className="table">
+                <table className="content-table">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Nome</th>
+                            <th>CPF</th>
+                            <th>Destino</th>
+                            <th>Data de partida</th>
+                            <th>Data de retorno</th> 
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{this.props.id}</td>
+                            <td>{this.props.nome}</td>
+                            <td>{this.props.cpf}</td>
+                            <td>{this.props.destino}</td>
+                            <td>{this.props.partida}</td>
+                            <td>{this.props.retorno}</td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </section>
     }
